561c0694aad2d3f77abc0153e083d11222230878
Paul Petrov <paul@bargamut.ru>
2020-03-29 23:12:11 +0300

    Union types ('type' option)

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,12 @@ let e: number = c as number;
 
 let f: any = `123`;
 let g: number = (<string>f).length;
+
+// Type union
+let h: number | string = 123;
+h = `123`;
+
+type unionType = number | string;
+
+let H: unionType = 123;
+H = `123`;

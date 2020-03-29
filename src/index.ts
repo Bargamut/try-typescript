85168e24f4d79045539a66aa4ef2bfc66e5cf101
Paul Petrov <paul@bargamut.ru>
2020-03-29 23:11:30 +0300

    Types conversion

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,14 @@
-function a (x: number, y: number): number {
-	return x + y;
-}
-const b = (x: number, y: number): number => x + y;
+let a: any = 1;
+let b: number = a;
 
-/**
- * Function Signature
- */
-let c: (x: number, y: number) => number;
+// Type conversion
+let c: unknown = 1;
+let d: number = <number>c;
 
-c = b;
+// same as
+let e: number = c as number;
 
-/**
- * Finction without return
- * 
- * @param {number} x
- * @param {number} y
- */
-let d = (x: number, y: number): void => {
-	console.log(x + y);
-};
+// ----------------------
 
-/**
- * Never ending function witout endpoint
- * 
- * @param {string} x
- */
-let e = (x: string): never => {
-	throw new Error(x);
-};
-
-/**
- * Reload functions
- */
-
-
-let f = (x: any, y: any): any => {
-	if (typeof x === 'number') {
-		return x + y;
-	} else if (typeof x === 'string') {
-		return `${x} ${y}`;
-	}
-};
-
-console.log(f(1, 2));
-console.log(f(`Ответ`, 3));
-
-function F (x: number, y: number): number;
-function F (x: string, y: number): string;
-function F (x: any, y: any): any {
-	if (typeof x === 'number') {
-		return x + y;
-	} else if (typeof x === 'string') {
-		return `${x} ${y}`;
-	}
-};
-
-console.log(F(1, 2));
-console.log(F(`Ответ`, 3));
+let f: any = `123`;
+let g: number = (<string>f).length;

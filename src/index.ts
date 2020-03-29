4c5b02d92ae68367085b08b07417b7cfccf61ba2
Paul Petrov <paul@bargamut.ru>
2020-03-30 02:35:56 +0300

    Intersecion Type

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,16 @@
-let a: any = 1;
-let b: number = a;
-
-// Type conversion
-let c: unknown = 1;
-let d: number = <number>c;
-
-// same as
-let e: number = c as number;
-
+// Intersection Type
 // ----------------------
 
-let f: any = `123`;
-let g: number = (<string>f).length;
+type A = { a: string } & { b: string };
 
-// Type union
-let h: number | string = 123;
-h = `123`;
+let a: A = { a: `123`, b: `123` };
 
-type unionType = number | string;
+// Optional items
+type B = { a: string } & { b?: string };
 
-let H: unionType = 123;
-H = `123`;
+let b: B = { a: `123`, b: `123` };
+// b = { a: `123` };
 
-// ----------------------
-
-type J = 1 | 2 | 3 | `a` | `b` | `c`;
-
-let j: J = 1;
-j = 2;
-j = 3;
-j = `a`;
-j = `b`;
-j = `c`;
-
-type K = { a: string } | { b: number };
-
-let l: K = { a: `123` };
-l = { b: 123 };
-l = { a: `123`, b: 123 };
+if (`b` in b) {
+	console.log(`Hello TypeScript!`);
+}

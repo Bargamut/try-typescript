27413877d13ebd9bb480b27317fe0fb876cca3e7
Paul Petrov <paul@bargamut.ru>
2020-03-30 17:13:54 +0300

    Corteges

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,8 @@ let AA: Array<number> = [ 1, 2, 3 ];
 
 let b: (number | string)[] = [ 1, 2, `3` ];
 let B: Array<number | string> = [ 1, 2, `3` ];
+
+// Corteges
+let c: [number, string] = [ 1, `2` ];
+let d: [number, string?] = [ 1, `2` ];
+d = [ 1 ];

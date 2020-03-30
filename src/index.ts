a050412ab6768b1edfd2d12386733d224cc62e73
Paul Petrov <paul@bargamut.ru>
2020-03-30 17:13:42 +0300

    Arrays

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
-// Intersection Type
-// ----------------------
+// Arrays
+let a = [ 1, 2, 3 ];
+let A: number[] = [ 1, 2, 3 ];
+let AA: Array<number> = [ 1, 2, 3 ];
 
-type A = { a: string } & { b: string };
-
-let a: A = { a: `123`, b: `123` };
-
-// Optional items
-type B = { a: string } & { b?: string };
-
-let b: B = { a: `123`, b: `123` };
-// b = { a: `123` };
-
-if (`b` in b) {
-	console.log(`Hello TypeScript!`);
-}
+let b: (number | string)[] = [ 1, 2, `3` ];
+let B: Array<number | string> = [ 1, 2, `3` ];

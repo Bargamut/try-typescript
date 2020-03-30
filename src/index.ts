d2632b2bcc33bafff782e21eafc5ab348c79c0f0
Paul Petrov <paul@bargamut.ru>
2020-03-30 17:56:45 +0300

    Enums

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,34 @@ let B: Array<number | string> = [ 1, 2, `3` ];
 let c: [number, string] = [ 1, `2` ];
 let d: [number, string?] = [ 1, `2` ];
 d = [ 1 ];
+
+// Enums
+enum E {
+	A,
+	B
+};
+
+console.log(E.A, E.B, E[0], E[1]);
+console.log(E[E.A], E[E.B]);
+
+enum EE {
+	A = 5,
+	B
+};
+
+console.log(EE.A, EE.B, EE[EE.A], EE[EE.B]);
+
+enum EEE {
+	A = `Hello`,
+	B = `TypeScript`
+};
+
+console.log(EEE.A, EEE.B);
+
+
+const enum F {
+	A,
+	B
+};
+
+console.log(F.A, F.B);
